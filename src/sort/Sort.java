bc3a049736b37cee13d44da74eaea5798e9596b8
Aung Kyaw Phyo <94116245+AungKyawPhyo1142@users.noreply.github.com>
2022-02-27 16:14:02 +0630

    Add files via upload

---
+++ src/sort/Sort.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sort;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Sort {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Frame obj = new Frame();
+    }
+    
+}
